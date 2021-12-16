37f65bb8997b1f1c22873775e0c0dbc297d389e9
MINJUNG-CHO <alswjd420663@gmail.com>
2021-12-16 13:51:15 +0900

    4

--- test/app.js
+++ test/app.js
@@ -3,3 +3,12 @@ const app = express();
 const path = require('path');
 
 app.use('/', express.static(path.join(__dirname, 'index.html')));
+
+app.get('/about', (req, res)=>{
+    res.send('안녕 나는 어바웃');
+});
+
+
+app.listen(app.get('port'), ()=>{
+    console.log('익스프레스 서버 실행');
+});
